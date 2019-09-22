41c73618910722ad4d7cec67f33629ad6fb560d0
Ricardo Oporto <roporto83@gmail.com>
2019-09-23 07:44:15 +1000

    detail notes

--- src/app/components/schedule-detail/schedule-detail.component.ts
+++ src/app/components/schedule-detail/schedule-detail.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { Schedule } from 'src/app/shared/models';
 
 @Component({
   selector: 'app-schedule-detail',
   templateUrl: './schedule-detail.component.html',
-  styleUrls: ['./schedule-detail.component.scss']
+  styleUrls: ['./schedule-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ScheduleDetailComponent implements OnInit {
   @Input() schedule: Schedule;
 
   public showDetail = false;
   public note: string;
+  public noteList = [];
 
   constructor() {}
 
   ngOnInit() {
     const x = '';
   }
+
+  addNote = note => {
+    if (!note) {
+      return;
+    }
+
+    this.noteList.push(this.note);
+    this.note = null;
+  };
 }
