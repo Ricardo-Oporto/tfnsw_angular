5b1a847b3cae8a0a37b6830a84d88daa9ed9ae21
Ricardo Oporto <roporto83@gmail.com>
2019-09-23 10:27:42 +1000

    Update running-time.directive.spec.ts

--- src/app/directives/runningTime/running-time.directive.spec.ts
+++ src/app/directives/runningTime/running-time.directive.spec.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 import { RunningTimeDirective } from './running-time.directive';
 
 describe('RunningTimeDirective', () => {
-  it(`should return On Time when the timestamp is greater than 0 and less that 300`, () => {
+  it(`should return On Time when running time is greater than 0 and less that 300`, () => {
     @Component({
       selector: 'app-test-component',
       template: `
@@ -12,7 +12,7 @@ describe('RunningTimeDirective', () => {
       `
     })
     class TestComponent {
-      public time = 200;
+      public time = 100;
     }
     let componentFixture: ComponentFixture<TestComponent>;
     TestBed.configureTestingModule({
@@ -23,5 +23,68 @@ describe('RunningTimeDirective', () => {
     componentFixture.detectChanges();
     const element = componentFixture.nativeElement.innerText;
     expect(element).toContain(Status.ON_TIME);
+  });
+
+  it(`should return On Time when running time is less than 0`, () => {
+    @Component({
+      selector: 'app-test-component',
+      template: `
+        <div appRunningTime [time]="time"></div>
+      `
+    })
+    class TestComponent {
+      public time = -100;
+    }
+    let componentFixture: ComponentFixture<TestComponent>;
+    TestBed.configureTestingModule({
+      declarations: [RunningTimeDirective, TestComponent]
+    });
+    TestBed.compileComponents();
+    componentFixture = TestBed.createComponent(TestComponent);
+    componentFixture.detectChanges();
+    const element = componentFixture.nativeElement.innerText;
+    expect(element).toContain(Status.EARLY);
+  });
+
+  it(`should return late when running time is greater than 300`, () => {
+    @Component({
+      selector: 'app-test-component',
+      template: `
+        <div appRunningTime [time]="time"></div>
+      `
+    })
+    class TestComponent {
+      public time = 400;
+    }
+    let componentFixture: ComponentFixture<TestComponent>;
+    TestBed.configureTestingModule({
+      declarations: [RunningTimeDirective, TestComponent]
+    });
+    TestBed.compileComponents();
+    componentFixture = TestBed.createComponent(TestComponent);
+    componentFixture.detectChanges();
+    const element = componentFixture.nativeElement.innerText;
+    expect(element).toContain(Status.LATE);
+  });
+
+  it(`should return unknown when running time unknown`, () => {
+    @Component({
+      selector: 'app-test-component',
+      template: `
+        <div appRunningTime [time]="time"></div>
+      `
+    })
+    class TestComponent {
+      public time = 'UNKNOWN';
+    }
+    let componentFixture: ComponentFixture<TestComponent>;
+    TestBed.configureTestingModule({
+      declarations: [RunningTimeDirective, TestComponent]
+    });
+    TestBed.compileComponents();
+    componentFixture = TestBed.createComponent(TestComponent);
+    componentFixture.detectChanges();
+    const element = componentFixture.nativeElement.innerText;
+    expect(element).toContain(Status.UNKNOWN);
   });
 });
