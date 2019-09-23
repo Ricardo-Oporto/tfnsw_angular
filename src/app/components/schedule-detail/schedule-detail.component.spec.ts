38e080fb796a1e944159a03cf438db49b2162f62
Ricardo Oporto <roporto83@gmail.com>
2019-09-23 12:07:12 +1000

    spec update

--- src/app/components/schedule-detail/schedule-detail.component.spec.ts
+++ src/app/components/schedule-detail/schedule-detail.component.spec.ts
@@ -1,8 +1,9 @@
 import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import response from '../../../assets/bus-services-data.json';
 import { ScheduleDetailComponent } from './schedule-detail.component';
 import { RunningTimeDirective } from 'src/app/directives/runningTime/running-time.directive';
+import { By } from '@angular/platform-browser';
 
 describe('ScheduleDetailComponent', () => {
   let component: ScheduleDetailComponent;
@@ -18,12 +19,22 @@ describe('ScheduleDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ScheduleDetailComponent);
     component = fixture.componentInstance;
-    component.schedule = { organisation: 'test', busData: [], date: 'sadasd' };
+    component.schedule = response.data[0];
+    component.showDetail = true;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open the detail view', async () => {
+    fixture.detectChanges();
+    setTimeout(() => {
+      component.showDetail = true;
+      const element = fixture.debugElement.queryAll(By.css('.schedule-detail'));
+      expect(element.length).toEqual(1);
+    }, 0);
   });
 
   it('should add a note to the note list', () => {
