5fbd52261b2e3cad553f5c02f4a96644230fb78e
Ricardo Oporto <roporto83@gmail.com>
2019-09-23 11:13:31 +1000

    Update schedule.component.spec.ts

--- src/app/components/schedule/schedule.component.spec.ts
+++ src/app/components/schedule/schedule.component.spec.ts
@@ -4,6 +4,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScheduleComponent } from './schedule.component';
 import { ScheduleDetailComponent } from '../schedule-detail/schedule-detail.component';
+import response from '../../../assets/bus-services-data.json';
+import { By } from '@angular/platform-browser';
 
 describe('ScheduleComponent', () => {
   let component: ScheduleComponent;
@@ -23,10 +25,17 @@ describe('ScheduleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ScheduleComponent);
     component = fixture.componentInstance;
+    component.scheduleList = response.data;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the schedules', () => {
+    fixture.detectChanges();
+    const de = fixture.debugElement.queryAll(By.css('app-schedule-detail'));
+    expect(de.length).toEqual(response.data.length);
   });
 });
