e7b720cc814218e1821c7ba5a8e1b21c73d9d9df
Ricardo Oporto <roporto83@gmail.com>
2019-09-23 10:51:50 +1000

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,41 @@
+import { DataService } from './services/data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Constants } from './shared/constants';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+
+  class MockData {
+    getBusRouteData() {
+      return of(data);
+    }
+  }
+
+  const data = [];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [AppComponent],
+      providers: [{ provide: DataService, useClass: MockData }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tfnsw'`, () => {
+  it('should get the data', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual(Constants.APP_TITLE);
-  });
+    app = fixture.debugElement.componentInstance;
+    app.ngOnInit();
+    expect(app.busRouteData).toEqual(data);
+  }));
 });
